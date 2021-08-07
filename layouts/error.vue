e5d0c1da8f0f082152d14f6836f2be63a1e5a867
Heather Nuffer <zzzuzz@gmail.com>
2021-08-06 22:04:23 -0600

    Tweaks styles in error page for better spacing

--- layouts/error.vue
+++ layouts/error.vue
@@ -6,7 +6,11 @@
       min-h-screen
       text-2xl
       items-center
-      justify-center
+      justify-start
+      pt-4
+      md:justify-center
+      px-4
+      md:px-auto
       mx-auto
       space-y-4
     "
@@ -14,15 +18,24 @@
     <h1 class="font-bold text-6xl">{{ error.statusCode }}</h1>
     <h1 v-if="error.statusCode === 404">Sorry, page not found.</h1>
     <h1 v-else>Sorry, an error occurred.</h1>
-    <NuxtLink to="/" class="underline font-bold"
+    <NuxtLink to="/" class="underline font-bold text-center"
       >Let's go back to the home page!</NuxtLink
     >
-    <img src="/images/egg-faces.jpg" alt="eggs with faces" class="rounded" />
+    <img
+      src="/images/egg-faces.jpg"
+      alt="eggs with faces"
+      class="rounded pt-4"
+    />
   </div>
 </template>
 
 <script>
 export default {
-  props: ['error'],
+  props: {
+    error: {
+      required: true,
+      type: Object,
+    },
+  },
 }
 </script>
